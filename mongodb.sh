167566432de0d411960e5bb532dac733d3f9510a
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-05 04:28:02 +0530

    mongodb_script_location

--- mongodb.sh
+++ mongodb.sh
@@ -3,6 +3,7 @@ cp ${script_location}/files/mongodb-roboshop.conf /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y
 
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl enable mongod
 systemctl start mongod
 
