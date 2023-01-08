92d4ee45177aeac2ec611ded9f8aa0adb850b157
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-08 20:16:53 +0530

    config modification

--- frontend.sh
+++ frontend.sh
@@ -10,16 +10,16 @@ else
 fi
 
 
-systemctl enable nginx &>>&{LOG}
-systemctl start nginx &>>&{LOG}
+systemctl enable nginx &>>${LOG}
+systemctl start nginx &>>${LOG}
 
-rm -rf /usr/share/nginx/html/* &>>&{LOG}
+rm -rf /usr/share/nginx/html/* &>>${LOG}
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>&{LOG}
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 
-cd /usr/share/nginx/html &>>&{LOG}
-unzip /tmp/frontend.zip &>>&{LOG}
+cd /usr/share/nginx/html &>>${LOG}
+unzip /tmp/frontend.zip &>>${LOG}
 
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>&{LOG}
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
-systemctl restart nginx &>>&{LOG}
+systemctl restart nginx &>>${LOG}
