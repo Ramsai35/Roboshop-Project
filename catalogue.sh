0b0a4a8a637c8d1e483715fe8c27760c8ff64133
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-09 10:08:38 +0530

    catalogue_script_location

--- catalogue.sh
+++ catalogue.sh
@@ -1,10 +1,10 @@
 source common.sh
 
-print_head "Creating Repo"
+print_head "creating Repo"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Install NodeJS"
 yum install nodejs -y &>>${LOG}
 status_check
 
@@ -15,55 +15,55 @@ print_head "Add Application User"
   fi
 status_check
 
-print_head "Creating Repo"
+print_head "Creating DIrectory"
 mkdir -p /app &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Download Web content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Clean Up"
 rm -rf /app/* &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Changr dir"
 cd /app &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "UNZIP"
 unzip /tmp/catalogue.zip &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Change DIR"
 cd /app &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "INSTALL NPM"
 npm install &>>${LOG}
 status_check
 
 
-print_head "Creating Repo"
+print_head "Change dir"
 cp ${script_location}/files/catalogue-roboshop.conf /etc/systemd/system/catalogue.service &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "daemonon Load"
 systemctl daemon-reload &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "enable catalogue"
 systemctl enable catalogue &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "start catalogue"
 systemctl start catalogue &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "Install mongodb"
 labauto mongodb-client &>>${LOG}
 status_check
 
-print_head "Creating Repo"
+print_head "schema Load"
 mongo --host mongodb.ramdevops35.online </app/schema/catalogue.js &>>${LOG}
 status_check
