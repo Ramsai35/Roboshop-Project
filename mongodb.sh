c4df02903b82f8d874542ddc5b7ec171bc466e21
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-01-09 09:01:19 +0530

    catalogue_script_location

--- mongodb.sh
+++ mongodb.sh
@@ -1,10 +1,25 @@
-script_location=$(pwd)
-cp ${script_location}/files/mongodb-roboshop.conf /etc/yum.repos.d/mongo.repo
+source common.sh
 
-yum install mongodb-org -y
+print_head "changing Dir of command"
+cp ${script_location}/files/mongodb-roboshop.conf /etc/yum.repos.d/mongo.repo &>>${LOG}
+status_check
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl enable mongod
-systemctl start mongod
+print_head "Install MongoDB"
+yum install mongodb-org -y &>>${LOG}
+status_check
 
-systemctl restart mongod
+print_head "Changing Port status"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${LOG}
+status_check
+
+print_head "Enable MONGOD"
+systemctl enable mongod &>>${LOG}
+status_check
+
+print_head "Start MONGOD"
+systemctl start mongod &>>${LOG}
+status_check
+
+print_head "Start MONGOD"
+systemctl restart mongod &>>${LOG}
+status_check
